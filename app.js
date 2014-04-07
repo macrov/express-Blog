1587f4b7825963c64a8b5d16c4b319d10a880117
MacrovGao <backfire.ghw@gmail.com>
2014-04-07 22:28:09 +0800

    add framework

--- app.js
+++ app.js
@@ -23,8 +23,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(app.router);
 
-app.get('/', routes.index);
-app.get('/users', users.list);
+routes(app);
 
 /// catch 404 and forwarding to error handler
 app.use(function(req, res, next) {
